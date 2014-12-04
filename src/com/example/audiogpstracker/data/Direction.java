a8603d9076cd00cad0ab84ddf8a99480c40f91d3
Vladislav Verbitskiy <vladislav.verbitskiy@tatemgames.com>
2014-12-04 10:47:27 +0200

    1. Compass logic changes

--- src/com/example/audiogpstracker/data/Direction.java
+++ src/com/example/audiogpstracker/data/Direction.java
@@ -1,5 +1,8 @@
 package com.example.audiogpstracker.data;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.example.audiogpstracker.Constants;
 import com.example.audiogpstracker.MainActivity;
 import com.example.audiogpstracker.utils.DmafManager;
@@ -13,7 +16,33 @@ public class Direction implements SensorEventListener, Constants {
 
 	private static final String LOG = "Direction";
 	
-	private float lastDegree = 0.f;
+	private static Direction instance;
+	
+	private Timer timer;
+	
+	private float lastDegree = 0.0f;
+	
+	private float currentDegree = 0.0f;
+	private float agoDegree = 0.0f;
+	private float averageDegree = 0.0f;
+	
+	private  Direction() {
+		timer = new Timer();
+		
+		timer.schedule(new LastDegreeTimer(), 0, 250);
+		timer.schedule(new CurrentDegreeTimer(), 0, 1);
+	}
+	
+	public Direction getInstance() {
+		if (instance == null)
+			instance = new Direction();
+		return instance;
+	}
+	
+	public void getCompassChange() {
+		averageDegree = currentDegree - agoDegree;
+	}
+	
 	
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -25,7 +54,7 @@ public class Direction implements SensorEventListener, Constants {
 	public void onSensorChanged(SensorEvent event) {
 		// TODO Auto-generated method stub
 		// get the angle around the z-axis rotated
-		if(event.sensor.getType()==directionSensor){
+		if(event.sensor.getType()==directionSensor) {
 			float degree = Math.round(event.values[0]);
 			displayText(Float.toString(degree) + " degrees \nlast: " +
 						Float.toString(lastDegree) + " \nCompass change: " +
@@ -43,4 +72,50 @@ public class Direction implements SensorEventListener, Constants {
     	Log.i(LOG, direction);
     }
 	
+	
+	private class CurrentDegreeTimer extends TimerTask implements SensorEventListener
+	{
+		private float returnedValue = 0.0f;
+		
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			if(event.sensor.getType()==directionSensor) {
+				returnedValue = Math.round(event.values[0]);
+			}		
+		}
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			
+		}
+
+		@Override
+		public void run() {
+			currentDegree = returnedValue;
+		}
+		
+	}
+	
+	private class LastDegreeTimer extends TimerTask implements SensorEventListener
+	{
+		private float returnedValue = 0.0f;
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			if(event.sensor.getType()==directionSensor) {
+				returnedValue = Math.round(event.values[0]);
+			}						
+		}
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+			
+			
+		}
+
+		@Override
+		public void run() {
+			agoDegree = returnedValue;
+		}		
+	}
 }
